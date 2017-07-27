f566982aeea7fc8413874a3ef94b7e1a95786d39
DJ <daniel.s.destefano@gmail.com>
2017-07-27 11:00:20 -0400

    Re-writing history wooo! before hooks
    after hooks
    
    before all hooks
    
    after all hooks

--- src/main/java/TestSuiteOne.java
+++ src/main/java/TestSuiteOne.java
@@ -1,9 +1,29 @@
-import org.junit.Test;
+import org.junit.*;
 
 public class TestSuiteOne {
+    @BeforeClass
+    public static void beforeClass(){
+        System.out.println("This is how you do before SUITE hooks");
+    }
+
+    @Before
+    public void beforeMethod(){
+        System.out.println("This is how you do before hooks");
+    }
 
     @Test
     public void testMethod(){
         System.out.println("Hello Test One!");
     }
+
+    @After
+    public void afterMethod(){
+        System.out.println("This is how you do after hooks");
+    }
+
+    @AfterClass
+    public static void afterClass(){
+        System.out.println("This is how you do after SUITE hooks");
+    }
+
 }
